c16a63c482a0588adeb809ce14a2089be692e792
Selihei <selina.heinrichlp@gmail.com>
2025-06-10 14:05:19 +0200

    added Test 2

--- main.dart
+++ main.dart
@@ -11,4 +11,13 @@ if(age >= movieAgeRating || hasParentalConsent && age >= movieAgeRating-2)
     print("Test 1 darf Film sehen");
 else
     print("Test 1 darf nicht Film sehen");
+
+//Test 2
+age = 13;
+hasParentalConsent = false;
+movieAgeRating = 16;
+if(age >= movieAgeRating || hasParentalConsent && age >= movieAgeRating-2)
+    print("Test 2 darf Film sehen");
+else
+    print("Test 2 darf nicht Film sehen");
 }
